314dd3b534bfc6b6354ba26fa19bcf8faffbcee6
Do Nhat Minh <mrordinaire@gmail.com>
2013-06-05 21:11:00 +0800

    added some logic

--- Main.hs
+++ Main.hs
@@ -1,2 +1,15 @@
+import System.Environment (getArgs, getProgName)
+import System.IO (IOMode (..), withFile, hGetContents)
+
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = do
+    args <- getArgs
+    if length args /= 1
+        then do
+            progName <- getProgName
+            putStrLn $ "Usage: " ++ progName ++ " <ldns-file>"
+        else
+            withFile (head args) ReadMode
+                     (\h -> do
+                        contents <- hGetContents h
+                        putStr contents)
